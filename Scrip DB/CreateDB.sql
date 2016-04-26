526c653bb1e7f1748cf8e510aa2ba87023ea2470
n.fernandez <nikin179@hotmail.com>
2016-04-26 17:47:00 -0300

    Se modifica la tabla de definicion de usuarios. Se empieza a definir que campos llevarà.

--- Scrip DB/CreateDB.sql
+++ Scrip DB/CreateDB.sql
@@ -2,4 +2,14 @@ drop database DBMASAJE if exists;
 
 create database DBMASAJE;
 
---create table USERDEFN
+/*
+create table USERDEFN (
+userid varchar(12),
+documentid varchar(30),
+doumentid varchar(16),
+firstname varchar(255),
+secodname varcahr(255),
+lastname varchar(255),
+birthday date,
+mail varchar(255));
+*/
